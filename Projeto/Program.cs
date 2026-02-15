b08181cca57fee552e2c83c75bf3d801210c1e29
José de Senna <josespnt1990@gmail.com>
2026-02-15 14:45:53 -0300

    feat: introduz classe Triangulo e aplica modelo orientado a objetos

--- Projeto/Program.cs
+++ Projeto/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Globalization;
+using Projeto.Src;
 
 namespace Projeto;
 
@@ -6,22 +7,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Entre com as medidas do triângulo X:");
-        double xA = double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
-        double xB = double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
-        double xC = double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
+        Triangulo x = new Triangulo();
+        Triangulo y = new Triangulo();
+        
+        Console.WriteLine("Entre com os dados do primeiro triângulo:");
+        x.Nome = Console.ReadLine()!;
+        x.A = double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
+        x.B = double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
+        x.C = double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
 
         Console.WriteLine();
-        Console.WriteLine("Entre com as medidas do triângulo Y:");
-        double yA = double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
-        double yB = double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
-        double yC = double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
+        Console.WriteLine("Entre com as medidas do segundo triângulo:");
+        y.Nome = Console.ReadLine()!;
+        y.A = double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
+        y.B = double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
+        y.C = double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
 
-        double xP = (xA + xB + xC) / 2.0;
-        double yP = (yA + yB + yC) / 2.0;
-
-        double xArea = Math.Sqrt(xP * (xP - xA) * (xP - xB ) * (xP - xC));
-        double yArea = Math.Sqrt(yP * (yP - yA) * (yP - yB ) * (yP - yC));
+        double xArea = x.Area();
+        double yArea = y.Area();
 
         Console.WriteLine();
         Console.WriteLine("Área de X: " + xArea.ToString("F4", CultureInfo.InvariantCulture));
